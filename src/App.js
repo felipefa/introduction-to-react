f2cec01a4449a49c33f59560f1d41e553c3c311f
Felipe Araujo <devaraujofelipe@gmail.com>
2019-09-15 15:33:09 -0300

    Importing images

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 import './App.css';
 
+import profile from './assets/teste.jpeg';
+
 const App = () => {
-  return <h1>Hello Rocketseat</h1>;
+  return <img width="200" src={profile} />;
 };
 
 export default App;
