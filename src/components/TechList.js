1dbf150e4313aaf0e418a15986f7f71940a1a383
Felipe Araujo <devaraujofelipe@gmail.com>
2019-09-22 21:58:44 -0300

    Using lifecycle methods

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -5,8 +5,27 @@ import TechItem from './TechItem';
 class TechList extends Component {
   state = {
     newTech: '',
-    techs: ['Node.js', 'ReactJS', 'React Native']
+    techs: []
   };
+
+  // Executado assim que o compoenente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado sempre que houver alteração nas props ou estado
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillUnmount() {}
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
